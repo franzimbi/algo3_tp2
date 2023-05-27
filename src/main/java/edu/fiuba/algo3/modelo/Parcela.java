303f8faa18fb6b90b9fa01028ad7d33a8f381cb1
Nicolas <marcelo_n97@outlook.es>
2023-05-27 16:52:41 -0300

    Update scripts

---
+++ src/main/java/edu/fiuba/algo3/modelo/Parcela.java
@@ -0,0 +1,5 @@
+package edu.fiuba.algo3.modelo;
+
+public interface Parcela {
+    boolean ubicar(Defensa defensa);
+}
--- src/main/test/edu/fiuba/algo3/entrega_1/Entrega1Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/Entrega1Test.java
@@ -1,8 +1,6 @@
 package edu.fiuba.algo3.entrega_1;
 
-import edu.fiuba.algo3.modelo.CreditosInsuficientesError;
-import edu.fiuba.algo3.modelo.Defensa;
-import edu.fiuba.algo3.modelo.Jugador;
+import edu.fiuba.algo3.modelo.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,17 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Entrega1Test {
     @Test
     public void Test01UnJugadorComienzaEnUnEstadoValido() {
-        Jugador jugador = new Jugador(20,100);
+        Jugador jugador = new Jugador(20, 100);
 
-        assertEquals(jugador.obtenerVida(),20);
+        assertEquals(jugador.obtenerVida(), 20);
 
-        assertEquals(jugador.obtenerCreditos(),100);
+        assertEquals(jugador.obtenerCreditos(), 100);
     }
 
     @Test
-    public void Test02UnaDefensaSeConstruyeEnElTiempoCorrecto(){
-        Jugador jugador = new Jugador(20,100);
-        Defensa defensa = Defensa.construirDefensa("torre blanca",jugador);
+    public void Test02UnaDefensaSeConstruyeEnElTiempoCorrecto() {
+        Jugador jugador = new Jugador(20, 100);
+        Defensa defensa = Defensa.construirDefensa("torre blanca", jugador);
 
         assertFalse(defensa.estaOperativa());
 
@@ -30,13 +28,29 @@ public class Entrega1Test {
     }
 
     @Test
-    public void Test03UnJugadorDebeTenerCreditosSuficientesParaConstruirUnaTorre(){
-        Jugador jugador = new Jugador(20,40);
+    public void Test03UnJugadorDebeTenerCreditosSuficientesParaConstruirUnaTorre() {
+        Jugador jugador = new Jugador(20, 40);
 
-        assertDoesNotThrow(()->Defensa.construirDefensa("torre plateada",jugador));
+        assertDoesNotThrow(() -> Defensa.construirDefensa("torre plateada", jugador));
 
-        Defensa defensa = Defensa.construirDefensa("torre plateada",jugador);
+        Defensa defensa = Defensa.construirDefensa("torre plateada", jugador);
 
-        assertThrows(CreditosInsuficientesError.class,()->Defensa.construirDefensa("torre plateada",jugador));
+        assertThrows(CreditosInsuficientesError.class, () -> Defensa.construirDefensa("torre plateada", jugador));
+    }
+
+    @Test
+    public void Test04SoloSePuedeConstruirDefensasSobreTierra() {
+        Jugador jugador = new Jugador(20, 100);
+
+        Defensa defensa = Defensa.construirDefensa("torre blanca", jugador);
+
+        Tierra tierra = new Tierra();
+        Rocoso rocoso = new Rocoso();
+        Pasarela pasarela = new Pasarela();
+
+        assertFalse(rocoso.ubicar(defensa));
+        assertFalse(pasarela.ubicar(defensa));
+        assertTrue(tierra.ubicar(defensa));
+
     }
 }
