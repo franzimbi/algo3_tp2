ce987cfe6dab904e787fb7a8195294d13c10bb8e
franzimbi <fzimbimbakis@fi.uba.ar>
2023-05-27 17:02:09 -0300

    actualizacion script render

--- scripts/render_diagrams.sh
+++ scripts/render_diagrams.sh
@@ -8,10 +8,18 @@ echo "Todos los archivos '*.png' dentro de \"$diagramsPath\" serán borrados."
 echo "Se regeneraran a partir de los archivos '*.plantuml'."
 echo
 
-read -t 3 -n 1 -p "Desea continuar (y/N)? " answer
+read -t 20 -n 1 -p "Desea continuar (y/N): " answer
 [ -z "$answer" ] && answer="n"
 
 if [ "$answer" == "y" ]
 then
-    find $diagramsPath -name '*.plantuml' -exec plantuml -v {} \;
+cd ..
+cd diagrams
+for i in classes packages sequences states
+do
+  cd $i
+  rm *png
+  plantuml -tpng *.puml
+  cd ..
+done
 fi
